f8f0451f536f5a677a6dc8237fa1758058c7857b
Cifer025 <41005657+Cifer025@users.noreply.github.com>
2020-08-05 17:16:46 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 mesa_install () {
-echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal main" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://apt.llvm.org/focal/ llvm-toolchain-focal main" | sudo tee -a /etc/apt/sources.list
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 # Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421 
 sudo apt update
