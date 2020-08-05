ebc3d2a616cf2d591034102f12e38c81a902fcfd
Cifer025 <41005657+Cifer025@users.noreply.github.com>
2020-08-05 15:17:58 -0400

    Update mesa-llvm.sh

--- mesa-llvm.sh
+++ mesa-llvm.sh
@@ -7,9 +7,9 @@ printf "1)Install\n2)Update\n3)Exit\nOptions:"
 read choice 
 if [ $choice -eq 1 ]; then 
     mesa_install
-elif [ $choice  -eq 2 ]; then
+elif [ $choice -eq 2 ]; then
    mesa_update
-elif [ $choice  -eq 3 ]; then
+elif [ $choice -eq 3 ]; then
     exit
 fi    
  
