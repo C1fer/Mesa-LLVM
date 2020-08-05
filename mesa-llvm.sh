a06bb47d346f4f0078760a2a71e0948934587a90
Cifer025 <41005657+Cifer025@users.noreply.github.com>
2020-08-05 14:28:35 -0400

    Add files via upload

---
+++ mesa-llvm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source ./functions.sh
+printf "A very basic mesa script\n\n"
+printf "Choose an option\n\n"
+printf "1)Install\n2)Update\n3)Exit\nOptions:"
+read choice 
+if [ $choice -eq 1 ]; 
+then
+    mesa_install
+elif [ $choice  -eq 2 ];
+then
+   mesa_update
+elif [ $choice  -eq 3 ];
+then
+    exit
+fi   
+
+
