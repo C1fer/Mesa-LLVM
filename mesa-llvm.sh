4ec265fb51f245bf218646e86bad89f5f1facb5f
Cifer025 <41005657+Cifer025@users.noreply.github.com>
2020-08-05 15:55:40 -0400

    Update mesa-llvm.sh

--- mesa-llvm.sh
+++ mesa-llvm.sh
@@ -2,7 +2,7 @@
 wget -q https://raw.githubusercontent.com/Cifer025/Mesa-LLVM/master/functions.sh
 source ./functions.sh
 printf "A very basic mesa script\n\n"
-printf "\e[4mOptions\n\n"
+printf "\e[4mOptions\n\n\e[0m"
 printf "1)Install\n2)Update\n3)Exit\n"
 read -p "Select the option: "  choice
 if [ $choice -eq 1 ]; then 
