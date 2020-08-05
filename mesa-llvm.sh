b34eaca71e5ec16da78419c575ac70e681d877e2
Cifer025 <41005657+Cifer025@users.noreply.github.com>
2020-08-05 16:00:02 -0400

    Update mesa-llvm.sh

--- mesa-llvm.sh
+++ mesa-llvm.sh
@@ -6,11 +6,11 @@ source ./functions.sh
 printf "\e[4mOptions\n\n\e[0m"
 printf "1)Install\n2)Update\n3)Exit\n"
 read -p "Select the option: "  choice
-if [ $choice = 1 ]; then 
+if [ $choice = "1" ]; then 
     mesa_install
-elif [ $choice = 2 ]; then
+elif [ $choice = "2" ]; then
    mesa_update
-elif [ $choice = 3 ]; then
+elif [ $choice = "3" ]; then
     exit
 fi    
 }
