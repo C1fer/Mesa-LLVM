b254675eb27d6c2521d193789bcccf038392df96
C1fer <41005657+C1fer@users.noreply.github.com>
2020-09-08 18:47:28 -0400

    Removed dir removal

--- mesa-llvm.sh
+++ mesa-llvm.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #Prepare installer
 if [ -d "/tmp/mesa-llvm" ]; then
- rm -rf /tmp/mesa-llvm
+  cd mesa-llvm
 else 
-   :
+  mkdir /tmp/mesa-llvm && cd /tmp/mesa-llvm
+  wget -q https://raw.githubusercontent.com/Cifer025/Mesa-LLVM/master/functions.sh
 fi
-cd /tmp && mkdir mesa-llvm
-cd mesa-llvm
-wget -q https://raw.githubusercontent.com/Cifer025/Mesa-LLVM/master/functions.sh
 source ./functions.sh
 #Start screen
 home () {
-printf "\nA very simple mesa + llvm installer\n\n"
+printf "\nA very simple mesa-git and llvm-git installer\n\n"
 printf "\e[1;4mOptions\n\n\e[0m"
 printf "1) Install\n2) Update\n3) Exit\n\n"
 read -p "Select an option: "  choice
